56aa9c3d25525908c26648ab20ce78c8f58d2df3
Idriz Altes <99037664+LetRookie@users.noreply.github.com>
2024-02-22 09:29:21 +0200

    Creater server and routes

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const routes = require('./routes');
+
+const app = express();
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+app.use(routes);
+
+app.listen(5000, console.log('Server is running on 5000...'));
