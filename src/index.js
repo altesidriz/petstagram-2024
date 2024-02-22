5ebcfa0c924be3d2dfebd62b2c29bc9326fc9467
Idriz Altes <99037664+LetRookie@users.noreply.github.com>
2024-02-22 10:36:06 +0200

    Added static views and styles

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const routes = require('./routes');
+const handlebars = require('express-handlebars');
+const path = require('path');
 
 const app = express();
 
-app.use(express.static('public'));
+//handlebars configuration
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', 'src/views')
+
+app.use(express.static(path.resolve(__dirname, 'public')));
 app.use(express.urlencoded({ extended: false }));
 app.use(routes);
 
