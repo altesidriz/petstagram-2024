9c2b4ea0c1d88b5e22a37a68789af12f39b013cc
Idriz Altes <99037664+LetRookie@users.noreply.github.com>
2024-02-25 17:54:44 +0200

    new photos model added

---
+++ src/models/Photo.js
@@ -0,0 +1,31 @@
+const mongoose = require ('mongoose');
+
+const photoSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    owner:{
+        type: mongoose.Types.ObjectId,
+        ref: 'User'
+    }
+});
+
+const User = mongoose.model('Photo', photoSchema);
+module.exports = User;
